d4ca6c74b3c4d0544af8e057ac62c67ad4471a12
Flávia Taho <flavia.taho@proton.me>
2024-02-06 08:10:14 -0300

    add truncate settings for telescope

--- lua/telescope-settings.lua
+++ lua/telescope-settings.lua
@@ -1,5 +1,11 @@
+require("telescope").setup({
+  defaults = {
+    file_ignore_patterns = {"node_modules", ".git"},
+    path_display = {"truncate"},
+  },
+})
+
 vim.api.nvim_set_keymap("n", "<C-p>", "<cmd>Telescope find_files<CR>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>Telescope buffers<CR>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", {noremap = true})
-
