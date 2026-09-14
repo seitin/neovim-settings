5e09b6e882dda2fa9f8534c6215731db77dd1186
Flavia <flavia.taho@proton.me>
2026-09-13 21:12:29 -0300

    add hour minute to git blame

--- lua/git.lua
+++ lua/git.lua
@@ -35,5 +35,5 @@ require("gitsigns").setup({
     virt_text_priority = 100,
   },
 
-  current_line_blame_formatter = "<author> | <author_time:%Y-%m-%d> - <summary>",
+  current_line_blame_formatter = "<author> | <author_time:%Y-%m-%d %H:%M> - <summary>",
 })
