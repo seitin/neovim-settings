eafd133836edea3e6f11f1e41010b0236a5d4a93
Flavia <flavia.taho@proton.me>
2026-09-13 21:11:50 -0300

    add gitsigns

--- lua/git.lua
+++ lua/git.lua
@@ -1,4 +1,3 @@
-vim.pack.add({ { src = "https://github.com/f-person/git-blame.nvim" } })
 vim.pack.add({
   {
     src = "https://github.com/NeogitOrg/neogit",
@@ -23,4 +22,18 @@ vim.pack.add({
       },
     },
   },
+})
+vim.pack.add({ { src = "https://github.com/lewis6991/gitsigns.nvim" } })
+require("gitsigns").setup({
+  current_line_blame = true,
+
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
+    delay = 50, -- Delay before blame virtual text displays (in ms)
+    ignore_whitespace = false,
+    virt_text_priority = 100,
+  },
+
+  current_line_blame_formatter = "<author> | <author_time:%Y-%m-%d> - <summary>",
 })
